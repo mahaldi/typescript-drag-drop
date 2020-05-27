c98137e10b75519ead26d10591b91df123c40df9
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-05-27 19:34:10 +0700

    display form

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,36 @@
 // Code goes here!
+
+class ProjectInput {
+    templeteElement: HTMLTemplateElement
+    hostElement: HTMLDivElement
+    element: HTMLFormElement
+    titleInputElement: HTMLInputElement
+    descriptionInputElement: HTMLInputElement
+    peopleInputElement: HTMLInputElement
+    constructor() {
+        this.templeteElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement
+
+        const importedNode = document.importNode(this.templeteElement.content, true)
+        this.element = importedNode.firstElementChild as HTMLFormElement // form tag line 13
+        this.element.id = 'user-input'
+
+        this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement
+        this.peopleInputElement = this.element.querySelector('#title')! as HTMLInputElement
+        this.descriptionInputElement = this.element.querySelector('#title')! as HTMLInputElement
+        this.configure()
+        this.attach()
+    }
+    private eventHandler(event: Event) {
+        event.preventDefault()
+        console.log(this.titleInputElement.value)
+    }
+    private configure() {
+        this.element.addEventListener('submit', this.eventHandler.bind(this))
+    }
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element)
+    }
+}
+
+const project = new ProjectInput()
