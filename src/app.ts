81169ae281708f06f6e49f4a130d66064e5881e3
mahaldiWS_ <aldi@umroh.com>
2020-05-28 12:27:59 +0700

    list item

--- src/app.ts
+++ src/app.ts
@@ -117,6 +117,25 @@ abstract class Component<T extends HTMLElement,U extends HTMLElement> { // make 
     abstract renderContent(): void
 }
 
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
+    private project: Project
+
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id)
+        this.project = project
+        this.configure()
+        this.renderContent()
+    }
+    configure() {
+
+    }
+    renderContent() {
+        this.element.querySelector('h2')!.textContent = this.project.title
+        this.element.querySelector('h3')!.textContent = this.project.people.toString()
+        this.element.querySelector('p')!.textContent = this.project.description
+    }
+}
+
 class ProjectList extends Component<HTMLDivElement, HTMLElement>{
     // templeteElement: HTMLTemplateElement
     // hostElement: HTMLDivElement
@@ -151,9 +170,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>{
         const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement
         listEl.innerHTML = ''
         for( const prjItem of this.assignedProjects) {
-            const listItem = document.createElement('li')
-            listItem.textContent = prjItem.title
-            listEl.appendChild(listItem)
+            // const listItem = document.createElement('li')
+            // listItem.textContent = prjItem.title
+            // listEl.appendChild(listItem)
+            new ProjectItem(this.element.querySelector('ul')!.id, prjItem)
         }
     }
 
