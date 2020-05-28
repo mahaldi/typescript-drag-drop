3ea0d450a1d9818ac9d6f4d19dff0f5676bbe4a4
mahaldiWS_ <aldi@umroh.com>
2020-05-28 16:08:40 +0700

    split code

---
+++ src/components/project-input.ts
@@ -0,0 +1,61 @@
+namespace App {
+    export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+        titleInputElement: HTMLInputElement
+        descriptionInputElement: HTMLInputElement
+        peopleInputElement: HTMLInputElement
+        constructor() {
+            super('project-input', 'app', true, 'user-input')
+    
+            this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement
+            this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement
+            this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement
+            this.configure()
+        }
+    
+        private gatherUserInput(): [string, string, number] | void { //[string, string, number] return value nya harus sesuai dengan urutan ini type nya
+            const centeredTitle = this.titleInputElement.value
+            const enteredDescription = this.descriptionInputElement.value
+            const enteredPeople = +this.peopleInputElement.value
+            const titleValidatable: Validatable = {
+                value: centeredTitle,
+                required: true
+            }
+            const descriptionValidatable: Validatable = {
+                value: enteredDescription,
+                required: true,
+                minLength: 5
+            }
+            const peopleValidatable: Validatable = {
+                value: enteredPeople,
+                required: true,
+                min: 1,
+                max: 5
+            }
+            if( !validate(titleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)) {
+                alert('not valid')
+            } else {
+                return [centeredTitle, enteredDescription, enteredPeople]
+            }
+        }
+        private clearInput() {
+            this.titleInputElement.value = ''
+            this.descriptionInputElement.value = ''
+            this.peopleInputElement.value = ''
+        }
+        @autobind
+        private submitHandler(event: Event) {
+            event.preventDefault()
+            const userInput = this.gatherUserInput()
+            if(Array.isArray(userInput)) { // karna tuple return array
+                const [title, description, people] = userInput // desctructur array
+                projectState.addProject(title, description, people)
+                this.clearInput()
+            }
+            
+        }
+        configure() {
+            this.element.addEventListener('submit', this.submitHandler)
+        }
+        renderContent(){}
+    }
+}
---
+++ src/components/project-list.ts
@@ -0,0 +1,61 @@
+namespace App {
+    export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget{
+        assignedProjects: Project[]
+        constructor(private type: 'active' | 'finished') {
+            super('project-list', 'app', false, `${type}-projects`)
+            this.assignedProjects = []
+            this.configure()
+            this.renderContent()
+        }
+        @autobind
+        dragOverHandler(event: DragEvent){
+            if(event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+                event.preventDefault() // default nya karna tidak allowed untuk dropping
+                const listEl = this.element.querySelector('ul')!
+                listEl.classList.add('droppable')
+            }
+    
+        }
+        @autobind
+        dragLeaveHandler(_: DragEvent){
+    
+            const listEl = this.element.querySelector('ul')!
+            listEl.classList.remove('droppable')
+    
+        }
+        @autobind
+        dropHandler(event: DragEvent) {
+            const prjId = event.dataTransfer!.getData('text/plain')
+            projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active: ProjectStatus.Finished)
+        }
+        configure() {
+            this.element.addEventListener('dragover', this.dragOverHandler)
+            this.element.addEventListener('dragleave', this.dragLeaveHandler)
+            this.element.addEventListener('drop', this.dropHandler)
+            projectState.addListener((projects: Project[])=>{
+                const relevantProject = projects.filter(prj => {
+                    if(this.type === 'active')
+                    return prj.status === ProjectStatus.Active
+                    return  prj.status === ProjectStatus.Finished
+                })
+                this.assignedProjects = relevantProject
+                this.renderProjects()
+            })
+        }
+        private renderProjects() {
+            const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement
+            listEl.innerHTML = ''
+            for( const prjItem of this.assignedProjects) {
+                new ProjectItem(this.element.querySelector('ul')!.id, prjItem)
+            }
+        }
+    
+        renderContent() {
+            const listId = `${this.type}-projects-list`
+            this.element.querySelector('ul')!.id = listId
+            this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + 'PROJECTS'
+        }
+    
+    }
+    
+}
