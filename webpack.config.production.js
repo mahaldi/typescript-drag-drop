46a6ffc36c290d1c2d54e63807ece26fc5ac38f9
mahaldiWS_ <aldi@umroh.com>
2020-05-29 16:04:03 +0700

    using wepack dev server

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,14 +1,14 @@
 const path = require('path') //node js module import
+const CleanPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/app.ts',
     output: {
         filename: 'bundle.js', //bebas bisa di hash juga check doc nya
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist'
+        path: path.resolve(__dirname, 'dist')
     },
-    devtool: 'inline-source-map',
+    devtool: 'none',
     module: {
         rules: [
             {
@@ -20,5 +20,8 @@ module.exports = {
     },
     resolve: {
         extensions: ['.ts', '.js']
-    }
+    },
+    plugins: [
+        new CleanPlugin.CleanWebpackPlugin() // MENG CLEAR DIST FOLDER
+    ]
 }
